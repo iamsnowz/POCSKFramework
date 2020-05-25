eff6dd7567f890531f89e7959c7186590ab3a6c7
iamsnowz <toom_sarawoot@hotmail.com>
2020-05-25 23:01:14 +0700

    update framework to xc framework

---
+++ POCSKFramework.xcframework/ios-x86_64-simulator/POCSKFramework.framework/Headers/POCSKFramework.h
@@ -0,0 +1,19 @@
+//
+//  POCSKFramework.h
+//  POCSKFramework
+//
+//  Created by Michael Abadi Santoso on 5/14/20.
+//  Copyright © 2020 Michael Abadi Santoso. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for POCSKFramework.
+FOUNDATION_EXPORT double POCSKFrameworkVersionNumber;
+
+//! Project version string for POCSKFramework.
+FOUNDATION_EXPORT const unsigned char POCSKFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <POCSKFramework/PublicHeader.h>
+
+
